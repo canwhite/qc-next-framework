471598b97ef517b3cb7111e346a2940ca7d16848
qiaochao <qiaochao@icourt.cc>
2023-12-17 15:59:02 +0800

    feat: route

--- src/app/api/redis/add/route.ts
+++ src/app/api/redis/add/route.ts
@@ -1,15 +1,13 @@
 import { NextResponse } from 'next/server';
 import redis from "../../../../redis";
 
-
-
 export const GET = async (req:Requet)=>{
-    const value = await redis.get('myKey');
-
+    const {searchParams} = new URL(req.url);
+    const value = 'myValue'; //这里从search params里取
+    redis.set('myKey', value);
     return NextResponse.json({
         success:true,
         message:"success",
         data:value,
     })
-
 }
